29abb42c1c271d8df2f99562a43269deac6881b1
Suryansh9304 <rajputana932004@gmail.com>
2025-11-09 16:20:09 +0530

    Deployment: Adding new build with vite config updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// **CRUCIAL FIX:** Sets the base path for assets and scripts.
+const REPO_NAME = 'Portfolio'; 
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: `/${REPO_NAME}/`, 
   plugins: [react()],
 })
